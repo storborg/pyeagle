01376bc2a94a18f56fe0bc1efc8b8c5ce9f03796
Scott Torborg <scott@cartlogic.com>
2014-02-03 03:09:47 -0800

    Flesh out geometry support, begin adding support for SVG rendering

--- pyeagle/geometry.py
+++ pyeagle/geometry.py
@@ -1,3 +1,9 @@
+import math
+
+from lxml.etree import tostring
+from lxml.builder import E
+
+
 class Wire(object):
     def __init__(self, start, end, width, layer, curve=None, cap=None):
         self.x1, self.y1 = start
@@ -25,6 +31,24 @@ class Wire(object):
                    curve=curve,
                    cap=node.attrib.get('cap'))
 
+    def bounding_box(self):
+        return ((min(self.x1, self.x2), min(self.y1, self.y2)),
+                (max(self.x1, self.x2), max(self.y1, self.y2)))
+
+    def to_svg(self, offset, scale):
+        offx, offy = offset
+
+        color = 'rgb(80, 80, 80)'
+        style = 'stroke:%s;stroke-width:%d' % (color, self.width * scale)
+
+        return [E.line(
+            x1=str((self.x1 + offx) * scale),
+            y1=str((self.y1 + offy) * scale),
+            x2=str((self.x2 + offx) * scale),
+            y2=str((self.y2 + offy) * scale),
+            style=style,
+        )]
+
 
 class SMD(object):
     def __init__(self, name, pos, size, layer):
@@ -46,6 +70,24 @@ class SMD(object):
                    size=(float(node.attrib['dx']),
                          float(node.attrib['dy'])),
                    layer=int(node.attrib['layer']))
+
+    def bounding_box(self):
+        return ((self.x - (self.dx / 2.0), self.y - (self.dy / 2.0)),
+                (self.x + (self.dx / 2.0), self.y + (self.dy / 2.0)))
+
+    def to_svg(self, offset, scale):
+        offsetx, offsety = offset
+
+        color = 'rgb(180, 0, 0)'
+        style = 'fill:%s' % color
+
+        return [E.rect(
+            x=str((self.x + offsetx - (self.dx / 2.0)) * scale),
+            y=str((self.y + offsety - (self.dy / 2.0)) * scale),
+            width=str(self.dx * scale),
+            height=str(self.dy * scale),
+            style=style,
+        )]
 
 
 class Text(object):
@@ -72,6 +114,14 @@ class Text(object):
                    layer=int(node.attrib['layer']),
                    ratio=ratio)
 
+    def bounding_box(self):
+        # FIXME Can we actually calculate this? May need to render text.
+        return ((self.x, self.y),
+                (self.x, self.y))
+
+    def to_svg(self, offset, scale):
+        return []
+
 
 class Rectangle(object):
     def __init__(self, start, end, layer):
@@ -91,6 +141,29 @@ class Rectangle(object):
                    end=(float(node.attrib['x2']),
                         float(node.attrib['y2'])),
                    layer=int(node.attrib['layer']))
+
+    def bounding_box(self):
+        return ((min(self.x1, self.x2), min(self.y1, self.y2)),
+                (max(self.x1, self.x2), max(self.y1, self.y2)))
+
+    def to_svg(self, offset, scale):
+        offsetx, offsety = offset
+
+        color = 'rgb(180, 0, 0)'
+        style = 'fill:%s' % color
+
+        x = min(self.x1, self.x2)
+        y = min(self.y1, self.y2)
+        width = abs(self.x2 - self.x1)
+        height = abs(self.y2 - self.y1)
+
+        return [E.rect(
+            x=str((x + offsetx - (width / 2.0)) * scale),
+            y=str((y + offsety - (height / 2.0)) * scale),
+            width=str(width * scale),
+            height=str(height * scale),
+            style=style,
+        )]
 
 
 class Pad(object):
@@ -112,6 +185,16 @@ class Pad(object):
                         float(node.attrib['y'])),
                    drill=float(node.attrib['drill']),
                    diameter=float(node.attrib.get('diameter', 0)))
+
+    def bounding_box(self):
+        margin = self.diameter / 2.0
+        return ((self.x - margin,
+                 self.y - margin),
+                (self.x + margin,
+                 self.y + margin))
+
+    def to_svg(self, offset, scale):
+        return []
 
 
 class Pin(object):
@@ -140,6 +223,14 @@ class Pin(object):
                    rotate=rotate,
                    visible=visible)
 
+    def bounding_box(self):
+        # FIXME Need to render things
+        return ((self.x, self.y),
+                (self.x, self.y))
+
+    def to_svg(self, offset, scale):
+        return []
+
 
 class Polygon(object):
     def __init__(self, width, layer, vertices=None):
@@ -161,6 +252,16 @@ class Polygon(object):
                    layer=int(node.attrib['layer']),
                    vertices=vertices)
 
+    def bounding_box(self):
+        margin = self.width / 2.0
+        return ((min(x for x, y in self.vertices) - margin,
+                 min(y for x, y in self.vertices) - margin),
+                (max(x for x, y in self.vertices) + margin,
+                 max(y for x, y in self.vertices) + margin))
+
+    def to_svg(self, offset, scale):
+        return []
+
 
 class Hole(object):
     def __init__(self, pos, drill):
@@ -176,6 +277,25 @@ class Hole(object):
         return cls(pos=(float(node.attrib['x']),
                         float(node.attrib['y'])),
                    drill=float(node.attrib['drill']))
+
+    def bounding_box(self):
+        margin = self.drill / 2.0
+        return ((self.x - margin,
+                 self.y - margin),
+                (self.x + margin,
+                 self.y + margin))
+
+    def to_svg(self, offset, scale):
+        offsetx, offsety = offset
+        color = 'rgb(20, 20, 20)'
+        style = 'fill:rgba(0, 0, 0, 0);stroke:%s;stroke-width:%d' % (color, 1)
+
+        return [E.circle(
+            r=str((self.drill / 2.0) * scale),
+            cx=str((self.x + offsetx) * scale),
+            cy=str((self.y + offsety) * scale),
+            style=style,
+        )]
 
 
 class Circle(object):
@@ -198,6 +318,23 @@ class Circle(object):
                    width=float(node.attrib['width']),
                    layer=int(node.attrib['layer']))
 
+    def bounding_box(self):
+        margin = self.radius + (self.width / 2.0)
+        return ((self.x - margin, self.y - margin),
+                (self.x + margin, self.y + margin))
+
+    def to_svg(self, offset, scale):
+        offsetx, offsety = offset
+        color = 'rgb(20, 20, 20)'
+        style = 'fill:rgba(0, 0, 0, 0);stroke:%s;stroke-width:%d' % (color, 1)
+
+        return [E.circle(
+            r=str(self.radius * scale),
+            cx=str((self.x + offsetx) * scale),
+            cy=str((self.y + offsety) * scale),
+            style=style,
+        )]
+
 
 class Geometry(object):
     def __init__(self, primitives=None):
@@ -211,8 +348,36 @@ class Geometry(object):
                          (Text, 'text'),
                          (Rectangle, 'rectangle'),
                          (Polygon, 'polygon'),
+                         (Hole, 'hole'),
                          (Pad, 'pad'),
                          (Pin, 'pin')]:
             for subnode in node.xpath(tag):
                 primitives.append(cls.from_xml(subnode))
         return primitives
+
+    def bounding_box(self):
+        startx = starty = endx = endy = 0
+        for primitive in self.primitives:
+            (x1, y1), (x2, y2) = primitive.bounding_box()
+            startx = min(startx, x1)
+            starty = min(starty, y1)
+            endx = max(endx, x2)
+            endy = max(endy, y2)
+        return (startx, starty), (endx, endy)
+
+    def to_svg(self, scale):
+        (startx, starty), (endx, endy) = self.bounding_box()
+        width = math.ceil((endx - startx) * scale)
+        height = math.ceil((endy - starty) * scale)
+
+        offset = (-startx, -starty)
+
+        children = []
+        for primitive in self.primitives:
+            children.extend(primitive.to_svg(offset, scale))
+
+        root = E.svg(
+            *children,
+            width=str(width),
+            height=str(height))
+        return tostring(root)
